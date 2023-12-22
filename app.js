5fc68c31e6715f2d22ff7500be75b377a2776ff9
yewon97 <dpdnjs9936@gmail.com>
2023-12-23 01:52:13 +0900

    content 출력하기

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+const container = document.getElementById("root");
 const ajax = new XMLHttpRequest();
+const content = document.createElement("div");
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
 
@@ -8,15 +10,28 @@ ajax.send();
 const newsFeed = JSON.parse(ajax.response);
 const ul = document.createElement("ul");
 
+window.addEventListener("hashchange", () => {
+  const id = location.hash.substring(1);
+  ajax.open("GET", CONTENT_URL.replace("@id", id), false);
+  ajax.send();
+
+  const newsContent = JSON.parse(ajax.response);
+
+  const title = document.createElement("h1");
+  title.innerHTML = newsContent.title;
+  content.appendChild(title);
+});
+
 for (let i = 0; i < 10; i++) {
   const li = document.createElement("li");
   const a = document.createElement("a");
 
-  a.href = "#";
+  a.href = `#${newsFeed[i].id}`;
   a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
 
   li.appendChild(a);
   ul.appendChild(li);
 }
 
-document.getElementById("root").appendChild(ul);
+container.appendChild(ul);
+container.appendChild(content);
